d363b151d07af2b377a2858b3f2651900a4be9c8
medobarakat <ahmedbarakat2401@gmail.com>
2022-08-06 14:07:58 +0200

    start working on forget password

--- src/screens/ForgetPassword.js
+++ src/screens/ForgetPassword.js
@@ -1,56 +1,123 @@
-import { View, Image, StyleSheet } from "react-native";
+import { View, Image, StyleSheet, ActivityIndicator, Text } from "react-native";
 import React, { useState } from "react";
 import { Input, Stack, Button, Pressable, Heading } from "native-base";
 import { useTranslation } from "react-i18next";
+import { Formik } from "formik";
+import { Api_url, forget_pass_api } from "../utilites/ApiConstants";
+import axios from "axios";
+import { Modal } from "native-base";
 
 const ForgetPassword = () => {
   const { t } = useTranslation();
-  const [number, setNumber] = useState();
+  const [error, setError] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const HandleForgetPassword = async (login_id) => {
+    const url = Api_url + forget_pass_api;
+    setShowModal(true);
+    axios
+      .post(url, {
+        login_id,
+      })
+      .then((res) => {
+        if (res && res.status == 200) {
+          console.log(res.data);
+          setShowModal(false);
+        }
+      })
+      .catch((err) => {
+        // setError(err.response.data.message);
+        console.log(err.response.data.message);
+        setShowModal(false);
+      });
+  };
   return (
-    <View style={styles.container}>
-      <View
-        style={{
-          marginTop: 180,
-          alignItems: "center",
-        }}
-      >
-        <Image
-          source={require("../images/logo.png")}
-          style={styles.logo}
-          resizeMode={"contain"}
-        />
-        <View>
-          <View style={styles.headerContainer}>
-            <Heading style={styles.heading} fontFamily={"Tajawal_500Medium"}>
-              {t("forgetpassword")}
-            </Heading>
-          </View>
-          <Stack space={4} w="100%" alignItems="center">
-            <Input
-              _text={{
-                fontSize: 15,
-              }}
-              width={350}
-              borderRadius={10}
-              type="number"
-              placeholder={t("phonenumber")}
+    <Formik
+      initialValues={{ username: "" }}
+      onSubmit={async (values) => await HandleForgetPassword(values.username)}
+    >
+      {({ handleChange, handleBlur, handleSubmit, values }) => (
+        <View style={styles.container}>
+          <View
+            style={{
+              marginTop: 180,
+              alignItems: "center",
+            }}
+          >
+            <Image
+              source={require("../images/logo.png")}
+              style={styles.logo}
+              resizeMode={"contain"}
             />
-          </Stack>
+            {/* start of modal */}
+            <Modal isOpen={showModal}>
+              <Modal.Content maxWidth="400px">
+                <Modal.Body>
+                  <View style={styles.centerizedCol}>
+                    <ActivityIndicator size="large" color="#0000ff" />
+                  </View>
+                </Modal.Body>
+              </Modal.Content>
+            </Modal>
+            {/* end of modal */}
+
+            <View>
+              <View style={styles.headerContainer}>
+                <Heading
+                  style={styles.heading}
+                  fontFamily={"Tajawal_500Medium"}
+                >
+                  {t("forgetpassword")}
+                </Heading>
+              </View>
+              <View>
+                <View>
+                  {error && (
+                    <View style={styles.errmessage}>
+                      <Text style={styles.errmessagetxt}>{error}</Text>
+                    </View>
+                  )}
+                </View>
+                <View>
+                  {error === undefined && (
+                    <View style={styles.errmessage}>
+                      <Text style={styles.errmessagetxt}>
+                        Check Your Connection and retry to log in{" "}
+                      </Text>
+                    </View>
+                  )}
+                </View>
+              </View>
+              <Stack space={4} w="100%" alignItems="center">
+                <Input
+                  _text={{
+                    fontSize: 15,
+                  }}
+                  width={350}
+                  borderRadius={10}
+                  placeholder={t("username")}
+                  onBlur={handleBlur("username")}
+                  value={values.username}
+                  onChangeText={handleChange("username")}
+                />
+              </Stack>
+            </View>
+            <Button
+              onPress={handleSubmit}
+              style={styles.But}
+              size="sm"
+              width={350}
+              height={10}
+              marginTop={45}
+              borderRadius={10}
+              backgroundColor={"#E56B1F"}
+              _text={{ fontSize: 15 }}
+            >
+              {t("send")}
+            </Button>
+          </View>
         </View>
-        <Button
-          style={styles.But}
-          size="sm"
-          width={350}
-          height={10}
-          marginTop={45}
-          borderRadius={10}
-          backgroundColor={"#E56B1F"}
-          _text={{ fontSize: 15 }}
-        >
-          {t("send")}
-        </Button>
-      </View>
-    </View>
+      )}
+    </Formik>
   );
 };
 const styles = StyleSheet.create({
@@ -70,6 +137,22 @@ const styles = StyleSheet.create({
   heading: {
     textAlign: "center",
     color: "#E56B1F",
+  },
+  centerizedCol: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  errmessage: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 10,
+  },
+  errmessagetxt: {
+    fontSize: 14,
+    fontFamily: "Tajawal_500Medium",
+    color: "red",
   },
 });
 
